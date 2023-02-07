fc40740709a2e39b7eeafa8397e8a7fb80de0cdd
Ben Jenkins <bdj746.bham@gmail.com>
2023-02-07 11:19:05 +0000

    Updated GranuPack post-processing script to calculate Hausner Ratio and Carr's Index

--- GranuPack/GranuPack Post-Processing/granupack_postprocessing.py
+++ GranuPack/GranuPack Post-Processing/granupack_postprocessing.py
@@ -4,20 +4,29 @@ import seaborn as sns
 
 
 # User defined variables
-data_folder_path = 'data5'  # Path to folder containing data
+data_folder_path = ''  # Path to folder containing data
 file_name = 'bulk_density.npy'  # Name of file containing bulk density data
 
 # Import data
 bulk_density_data = np.load(f"{data_folder_path}/{file_name}")
 all_taps = np.linspace(0, len(bulk_density_data), len(bulk_density_data)+1)
-all_bulk_density = bulk_density_data
+
+# Calculate the Hausner Ratio
+hausner_ratio = bulk_density_data[-1]/bulk_density_data[0]
+
+# Calculate Carr's Compressibility Index
+carr_compressibility_index = ((bulk_density_data[-1] - bulk_density_data[0])/bulk_density_data[-1])*100
+
+# Print results
+print(f"Hausner Ratio: {hausner_ratio}")
+print(f"Carr's Compressibility Index: {carr_compressibility_index}")
 
 # Start graph
 fig = go.Figure()
 colours = sns.color_palette("colorblind")
 colours = colours.as_hex()
 fig.add_trace(go.Scatter(x=all_taps,
-                         y=all_bulk_density,
+                         y=bulk_density_data,
                          mode='markers',  # or lines
                          marker=dict(size=8, color=colours[0]),  # Change colours value per trace added
                          )
